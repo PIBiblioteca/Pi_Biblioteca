5c5d3eab011e316eef4761e3acd1d97f48840562
Kevin Henrique <kevinhleonardi@gmail.com>
2021-08-17 11:46:08 -0300

    .

--- BANCO_TESTE/index.php
+++ BANCO_TESTE/index.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Teste banco de dados</title>
 </head>
 
@@ -13,7 +15,7 @@
         $nome = $_POST["nome"];
         $email = $_POST["email"];
 
-        $sql = $con->prepare("INSERT INTO alunos(ra, nome, email) VALUES($ra, $nome, $email)");
+        $sql = $con->prepare("INSERT INTO alunos(ra, nome, email) VALUES('$ra', '$nome', '$email')");
         $sql -> execute();
         
         if($sql):
