839e02f109cb7f20941def0f56171ef3d2952dd6
Kevin Henrique <kevinhleonardi@gmail.com>
2021-08-20 16:32:19 -0300

    f

--- Testes/BANCO_TESTE/config.php
+++ Testes/BANCO_TESTE/config.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 $hostname = "localhost";
 $user = "root";
 $password = "";
@@ -9,5 +10,16 @@ $conexao = mysqli_connect($hostname, $user, $password, $database);
 if(!$conexao){
     print "Falha na conexão com o Banco de Dados";
 }
+=======
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$bd = "bibliofateca";
+
+$mysqli = new mysqli($host, $usuario, $senha, $bd);
+
+if($mysqli->connect_errno)
+    echo "Falha na conexão: (".$mysqli->connect_errno.") ".$mysqli->connect_error;
+>>>>>>> parent of 30f7e61 (Organização dos testes)
 
 ?>
